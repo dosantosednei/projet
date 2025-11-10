e65ed7567458c5d14ab81d7d0266a2ea65e0086e
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-11-10 20:42:15 -0300

    Add files via upload

---
+++ projetoleds/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace projetoleds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLed1 = new System.Windows.Forms.Button();
+            this.btnLed2 = new System.Windows.Forms.Button();
+            this.btnLed3 = new System.Windows.Forms.Button();
+            this.txtDadoDec = new System.Windows.Forms.TextBox();
+            this.txtDadoBin = new System.Windows.Forms.TextBox();
+            this.txtDadoHex = new System.Windows.Forms.TextBox();
+            this.btnLed4 = new System.Windows.Forms.Button();
+            this.btnLed5 = new System.Windows.Forms.Button();
+            this.btnLed7 = new System.Windows.Forms.Button();
+            this.btnLed6 = new System.Windows.Forms.Button();
+            this.btnLed8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLed1
+            // 
+            this.btnLed1.Location = new System.Drawing.Point(0, 366);
+            this.btnLed1.Name = "btnLed1";
+            this.btnLed1.Size = new System.Drawing.Size(75, 23);
+            this.btnLed1.TabIndex = 0;
+            this.btnLed1.Text = "1";
+            this.btnLed1.UseVisualStyleBackColor = true;
+            this.btnLed1.Click += new System.EventHandler(this.btnLed1_Click);
+            // 
+            // btnLed2
+            // 
+            this.btnLed2.Location = new System.Drawing.Point(81, 366);
+            this.btnLed2.Name = "btnLed2";
+            this.btnLed2.Size = new System.Drawing.Size(75, 23);
+            this.btnLed2.TabIndex = 1;
+            this.btnLed2.Text = "2";
+            this.btnLed2.UseVisualStyleBackColor = true;
+            this.btnLed2.Click += new System.EventHandler(this.btnLed2_Click);
+            // 
+            // btnLed3
+            // 
+            this.btnLed3.Location = new System.Drawing.Point(162, 366);
+            this.btnLed3.Name = "btnLed3";
+            this.btnLed3.Size = new System.Drawing.Size(75, 23);
+            this.btnLed3.TabIndex = 2;
+            this.btnLed3.Text = "3";
+            this.btnLed3.UseVisualStyleBackColor = true;
+            this.btnLed3.Click += new System.EventHandler(this.btnLed3_Click);
+            // 
+            // txtDadoDec
+            // 
+            this.txtDadoDec.Location = new System.Drawing.Point(39, 12);
+            this.txtDadoDec.Name = "txtDadoDec";
+            this.txtDadoDec.Size = new System.Drawing.Size(100, 20);
+            this.txtDadoDec.TabIndex = 3;
+            // 
+            // txtDadoBin
+            // 
+            this.txtDadoBin.Location = new System.Drawing.Point(222, 12);
+            this.txtDadoBin.Name = "txtDadoBin";
+            this.txtDadoBin.Size = new System.Drawing.Size(100, 20);
+            this.txtDadoBin.TabIndex = 4;
+            // 
+            // txtDadoHex
+            // 
+            this.txtDadoHex.Location = new System.Drawing.Point(384, 15);
+            this.txtDadoHex.Name = "txtDadoHex";
+            this.txtDadoHex.Size = new System.Drawing.Size(100, 20);
+            this.txtDadoHex.TabIndex = 5;
+            // 
+            // btnLed4
+            // 
+            this.btnLed4.Location = new System.Drawing.Point(247, 366);
+            this.btnLed4.Name = "btnLed4";
+            this.btnLed4.Size = new System.Drawing.Size(75, 23);
+            this.btnLed4.TabIndex = 6;
+            this.btnLed4.Text = "4";
+            this.btnLed4.UseVisualStyleBackColor = true;
+            this.btnLed4.Click += new System.EventHandler(this.btnLed4_Click);
+            // 
+            // btnLed5
+            // 
+            this.btnLed5.Location = new System.Drawing.Point(328, 366);
+            this.btnLed5.Name = "btnLed5";
+            this.btnLed5.Size = new System.Drawing.Size(75, 23);
+            this.btnLed5.TabIndex = 7;
+            this.btnLed5.Text = "5";
+            this.btnLed5.UseVisualStyleBackColor = true;
+            this.btnLed5.Click += new System.EventHandler(this.btnLed5_Click);
+            // 
+            // btnLed7
+            // 
+            this.btnLed7.Location = new System.Drawing.Point(490, 366);
+            this.btnLed7.Name = "btnLed7";
+            this.btnLed7.Size = new System.Drawing.Size(75, 23);
+            this.btnLed7.TabIndex = 8;
+            this.btnLed7.Text = "7";
+            this.btnLed7.UseVisualStyleBackColor = true;
+            this.btnLed7.Click += new System.EventHandler(this.btnLed7_Click);
+            // 
+            // btnLed6
+            // 
+            this.btnLed6.Location = new System.Drawing.Point(409, 366);
+            this.btnLed6.Name = "btnLed6";
+            this.btnLed6.Size = new System.Drawing.Size(75, 23);
+            this.btnLed6.TabIndex = 9;
+            this.btnLed6.Text = "6";
+            this.btnLed6.UseVisualStyleBackColor = true;
+            this.btnLed6.Click += new System.EventHandler(this.btnLed6_Click);
+            // 
+            // btnLed8
+            // 
+            this.btnLed8.Location = new System.Drawing.Point(571, 366);
+            this.btnLed8.Name = "btnLed8";
+            this.btnLed8.Size = new System.Drawing.Size(75, 23);
+            this.btnLed8.TabIndex = 10;
+            this.btnLed8.Text = "8";
+            this.btnLed8.UseVisualStyleBackColor = true;
+            this.btnLed8.Click += new System.EventHandler(this.btnLed8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(512, 202);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox2.Location = new System.Drawing.Point(360, 202);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 117);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox3.Location = new System.Drawing.Point(205, 202);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 117);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox4.Location = new System.Drawing.Point(66, 202);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 117);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox5.Location = new System.Drawing.Point(512, 55);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox6.Location = new System.Drawing.Point(360, 55);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 16;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox7.Location = new System.Drawing.Point(205, 55);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 17;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::projetoleds.Properties.Resources.download;
+            this.pictureBox8.Location = new System.Drawing.Point(66, 55);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 18;
+            this.pictureBox8.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(658, 423);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLed8);
+            this.Controls.Add(this.btnLed6);
+            this.Controls.Add(this.btnLed7);
+            this.Controls.Add(this.btnLed5);
+            this.Controls.Add(this.btnLed4);
+            this.Controls.Add(this.txtDadoHex);
+            this.Controls.Add(this.txtDadoBin);
+            this.Controls.Add(this.txtDadoDec);
+            this.Controls.Add(this.btnLed3);
+            this.Controls.Add(this.btnLed2);
+            this.Controls.Add(this.btnLed1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLed1;
+        private System.Windows.Forms.Button btnLed2;
+        private System.Windows.Forms.Button btnLed3;
+        private System.Windows.Forms.TextBox txtDadoDec;
+        private System.Windows.Forms.TextBox txtDadoBin;
+        private System.Windows.Forms.TextBox txtDadoHex;
+        private System.Windows.Forms.Button btnLed4;
+        private System.Windows.Forms.Button btnLed5;
+        private System.Windows.Forms.Button btnLed7;
+        private System.Windows.Forms.Button btnLed6;
+        private System.Windows.Forms.Button btnLed8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+    }
+}
+
---
+++ projetoleds/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static projetoleds.leds;
+
+namespace projetoleds
+{
+    public partial class Form1 : Form
+    {
+        private Leds leds;
+        public Form1()
+        {
+            InitializeComponent();
+            leds = new Leds();
+            atualizaInterface();
+        }
+
+        private void atualizaInterface()
+        {
+            txtDadoDec.Text = leds.getDado().ToString();
+            txtDadoBin.Text = Convert.ToString(leds.getDado(), 2);
+            txtDadoHex.Text = Convert.ToString(leds.getDado(), 16).ToUpper();
+            btnLed1.Text = (leds.getLed(1) ? "Off" : "On");
+            btnLed2.Text = (leds.getLed(2) ? "Off" : "On");
+            btnLed3.Text = (leds.getLed(3) ? "Off" : "On");
+            btnLed4.Text = (leds.getLed(4) ? "Off" : "On");
+            btnLed5.Text = (leds.getLed(5) ? "Off" : "On");
+            btnLed6.Text = (leds.getLed(6) ? "Off" : "On");
+            btnLed7.Text = (leds.getLed(7) ? "Off" : "On");
+            btnLed8.Text = (leds.getLed(8) ? "Off" : "On");
+            pictureBox1.Image = (leds.getLed(1) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox2.Image = (leds.getLed(2) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox3.Image = (leds.getLed(3) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox4.Image = (leds.getLed(4) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox5.Image = (leds.getLed(5) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox6.Image = (leds.getLed(6) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox7.Image = (leds.getLed(7) ? Properties.Resources.download_acesa : Properties.Resources.download);
+            pictureBox8.Image = (leds.getLed(8) ? Properties.Resources.download_acesa : Properties.Resources.download);
+        }
+
+        private void btnLed1_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(1))
+            {
+                leds.apagar(1);
+            }
+            else
+            {
+                leds.acender(1);
+            }
+            atualizaInterface();
+        }
+
+        private void btnLed2_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(2))
+            {
+                leds.apagar(2);
+            }
+            else
+            {
+                leds.acender(2);
+            }
+            atualizaInterface();
+        }
+
+        private void btnLed3_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(3))
+            {
+                leds.apagar(3);
+            }
+            else
+            {
+                leds.acender(3);
+            }
+            atualizaInterface();
+        }
+        private void btnLed4_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(4))
+            {
+                leds.apagar(4);
+            }
+            else
+            {
+                leds.acender(4);
+            }
+            atualizaInterface();
+        }
+        private void btnLed5_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(5))
+            {
+                leds.apagar(5);
+            }
+            else
+            {
+                leds.acender(5);
+            }
+            atualizaInterface();
+        }
+        private void btnLed6_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(6))
+            {
+                leds.apagar(6);
+            }
+            else
+            {
+                leds.acender(6);
+            }
+            atualizaInterface();
+        }
+        private void btnLed7_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(7))
+            {
+                leds.apagar(7);
+            }
+            else
+            {
+                leds.acender(7);
+            }
+            atualizaInterface();
+        }
+        private void btnLed8_Click(object sender, EventArgs e)
+        {
+            if (leds.getLed(8))
+            {
+                leds.apagar(8);
+            }
+            else
+            {
+                leds.acender(8);
+            }
+            atualizaInterface();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+       
+        
+}
